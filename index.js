3cd0646adc1511124494902934097d0733153792
Mohaiminul375 <mohaiminul375@gmail.com>
2025-02-24 10:54:03 +0600

    create server and connect to mongodb

---
+++ index.js
@@ -0,0 +1,53 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express();
+const port = 5000;
+require('dotenv').config();
+const cors = require('cors');
+// Middleware & Cors
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:3000',],
+    credentials: true
+}));
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixszr3u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+// test server
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
+    console.log(`fingo-server app listening on port ${port}`);
+});
