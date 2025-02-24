edcdb534bfc938f34616e4d57e40de73050e4923
Mohaiminul375 <mohaiminul375@gmail.com>
2025-02-24 18:58:49 +0600

    create user and get list

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = 5000;
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 const cors = require('cors');
 // Middleware & Cors
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000',],
+    origin: ['http://localhost:3000'],
     credentials: true
 }));
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixszr3u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -23,7 +24,93 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-    
+        // DB collection 
+        const usersCollections = client.db('fingo-mfs').collection('all-users');
+
+
+
+        // Users management and Authentication
+        // Create a User
+        app.post('/create-user', async (req, res) => {
+            try {
+                const { name, phone_number, PIN, email, userType, NID } = req.body;
+                console.log(req.body)
+                // Check for existing user
+                const existingUser = await usersCollections.findOne({
+                    $or: [
+                        { phone_number },
+                        { email },
+                        { NID }
+                    ]
+                });
+
+                if (existingUser) {
+                    return res.status(400).json({
+                        success: false,
+                        message: "Phone number, email, or NID already exists."
+                    });
+                }
+
+                // Hash the PIN (password)
+                const hashedPIN = bcrypt.hashSync(PIN, 10);
+
+                // Create user object
+                const newUser = {
+                    name,
+                    phone_number,
+                    email,
+                    userType,
+                    NID,
+                    PIN: hashedPIN,
+                    createdAt: new Date()
+                };
+
+                // Set account status & balance
+                if (userType === "Agent") {
+                    newUser.account_status = "Pending";
+                    newUser.current_balance = 100000;
+                } else if (userType === "User") {
+                    newUser.account_status = "Active";
+                    newUser.current_balance = 40;
+                }
+                console.log(newUser, 'before insert')
+                // Insert new user
+                const result = await usersCollections.insertOne(newUser);
+                res.status(201).json({
+                    success: true,
+                    message: "User registered successfully!",
+                    userId: result.insertedId
+                });
+
+            } catch (error) {
+                if (error.code === 11000) {
+                    return res.status(400).json({
+                        success: false,
+                        message: "Duplicate entry: Phone number, email, or NID already exists."
+                    });
+                }
+                console.error("Error registering user:", error);
+                res.status(500).json({
+                    success: false,
+                    message: "Internal server error."
+                });
+            }
+        });
+        // TODO: jwt secure
+        app.get('/all-users', async (req, res) => {
+            try {
+                const result = await usersCollections.find().toArray();
+                res.status(200).send(result)
+            } catch (error) {
+                console.error('Error fetching users:', error);
+                res.status(500).json({
+                    success: false,
+                    message: 'Failed to fetch users list. Please try again later.',
+                });
+            }
+
+        })
+
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
